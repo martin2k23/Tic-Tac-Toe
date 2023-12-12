66b8a826aa08acb85d4a77bbc2cdd170cc62b7be
martin2k23 <manidutt810@gmail.com>
2023-12-12 17:25:31 +0530

    changes

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import circleIcon from './Images/circle.png';
 import crossIcon from './Images/cross.png';
 import './App.css';
@@ -14,34 +14,41 @@ function App() {
   const [lock, setLock] = useState(false);
   const [winner, setWinner] = useState(null);
 
-
-  const checkWinner = () => {
+  const checkWinner = useCallback(() => {
     for (let i = 0; i < 3; i++) {
       if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
         setWinner(board[i][0]);
+        setLock(true);
         return;
       }
       if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
         setWinner(board[0][i]);
+        setLock(true);
         return;
       }
     }
 
     if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
       setWinner(board[0][0]);
+      setLock(true);
       return;
     }
 
     if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
       setWinner(board[0][2]);
+      setLock(true);
       return;
     }
+
     if (!board.flat().includes(null)) {
       setWinner('Tie');
+      setLock(true);
     }
-  };
+  },[board]);
 
-
+  useEffect(() => {
+    checkWinner();
+  }, [board, checkWinner]); 
 
   const toggle = (row, col) => {
     if (lock || board[row][col] || winner) {
@@ -55,7 +62,6 @@ function App() {
     });
 
     setCount(prevCount => prevCount + 1);
-    checkWinner();
   };
 
   const resetBoard = () => {
@@ -89,11 +95,12 @@ function App() {
       </div>
 
       {winner && (
-        <div className={`winner ${winner === 'Tie' ? 'celebrate' : ''}`}>
-          {winner === 'Tie' ? 'It\'s a Tie!' : `Player ${winner.toUpperCase()} wins!`}
+        <div className="winner">
+          <div className="title_scroll">
+              {winner === 'Tie' ? 'IT\'s a Tie!' : `Player ${winner.toUpperCase()} wins!`}
+          </div>
         </div>
       )}
-
 
       <button className="button" onClick={resetBoard}>
         Reset
