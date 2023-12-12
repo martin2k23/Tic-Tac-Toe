d24a6f0b195ac977d10f8390546dd75a17932e08
martin2k23 <manidutt810@gmail.com>
2023-12-12 15:18:44 +0530

    changes

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,10 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import circleIcon from './Images/circle.png';
+import crossIcon from './Images/cross.png';
 import './App.css';
 
+const iconMapping = {
+  'x': `<img src="${crossIcon}" alt="cross"/>`,
+  'o': `<img src="${circleIcon}" alt="circle"/>`,
+};
+
 function App() {
+  const [board, setBoard] = useState(Array.from({ length: 3 }, () => Array(3).fill(null)));
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+  const [winner, setWinner] = useState(null);
+
+
+  const checkWinner = () => {
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        setWinner(board[i][0]);
+        return;
+      }
+      if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        setWinner(board[0][i]);
+        return;
+      }
+    }
+
+    if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      setWinner(board[0][0]);
+      return;
+    }
+
+    if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+      setWinner(board[0][2]);
+      return;
+    }
+    if (!board.flat().includes(null)) {
+      setWinner('Tie');
+    }
+  };
+
+
+
+  const toggle = (row, col) => {
+    if (lock || board[row][col] || winner) {
+      return;
+    }
+
+    setBoard(prevBoard => {
+      const newBoard = [...prevBoard];
+      newBoard[row][col] = count % 2 === 0 ? 'x' : 'o';
+      return newBoard;
+    });
+
+    setCount(prevCount => prevCount + 1);
+    checkWinner();
+  };
+
+  const resetBoard = () => {
+    setBoard(Array.from({ length: 3 }, () => Array(3).fill(null)));
+    setCount(0);
+    setLock(false);
+    setWinner(null);
+  };
+
+  const renderBoard = () => {
+    return board.map((row, rowIndex) => (
+      <div key={rowIndex} className={`row${rowIndex + 1}`}>
+        {(row || []).map((box, colIndex) => (
+          <div
+            key={colIndex}
+            className="boxes"
+            onClick={() => toggle(rowIndex, colIndex)}
+            dangerouslySetInnerHTML={{ __html: iconMapping[box] || '' }}
+          />
+        ))}
+      </div>
+    ));
+  };
+
   return (
     <div className="App">
-      djlkn
+      <div className="title">Tic Tac Toe Game</div>
+
+      <div className={`board ${winner ? 'celebrate' : ''}`}>
+        {renderBoard()}
+      </div>
+
+      {winner && (
+        <div className={`winner ${winner === 'Tie' ? 'celebrate' : ''}`}>
+          {winner === 'Tie' ? 'It\'s a Tie!' : `Player ${winner.toUpperCase()} wins!`}
+        </div>
+      )}
+
+
+      <button className="button" onClick={resetBoard}>
+        Reset
+      </button>
     </div>
   );
 }
